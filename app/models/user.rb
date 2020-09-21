d03dc4141b949d0b0e7cde1a130fe1156a95fae8
kanta <k.kanta0826@gmail.com>
2020-09-21 11:00:57 +0000

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,7 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+.)+[a-z]{2,})\z/i}
   validates :password, presence: true
-  validates :password, length: { in: 8..32 }
-  validates :password, format: { with: /\A(?=.&#042;?[a-z])(?=.&#042;?\d)[a-z\d]+\z/i }
-  validates :password_confirmation, confirmation: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i }
   
   has_secure_password
 end
